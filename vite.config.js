8426f661c82a589958ea93d3d6a4e12441c116ba
Léonard Schafer <79663902+Sprudeel@users.noreply.github.com>
2023-03-22 21:17:50 +0000

    feat(stack): shit works now

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
+const host = 'app.test'; 
+
 export default defineConfig({
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
