a6983f3beb16348e870ebb5eee226ec0b79394ab
Léonard Schafer <79663902+Sprudeel@users.noreply.github.com>
2023-03-23 21:46:55 +0000

    feat(groups): add groups on backend

---
+++ app/Models/Group.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Group extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'aemtli'];
+
+    public function aemtli(): BelongsTo {
+        return $this->belongsTo(Aemtli::class);
+    }
+
+    public function participants(): HasMany {
+        return $this->hasMany(Participant::class);
+    }
+}
