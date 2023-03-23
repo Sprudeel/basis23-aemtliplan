f3d4cce2a137fcea7037485ac5206413746ba42e
Léonard Schafer <79663902+Sprudeel@users.noreply.github.com>
2023-03-23 21:48:22 +0000

    feat(routes): add aemtli routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\AemtliController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,6 +34,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    /**
+     * Ämtli
+     */
+    Route::get('/aemtli', [AemtliController::class, 'index'])->name('aemtli.index');
+    Route::post('/aemtli/store', [AemtliController::class, 'store'])->name('aemtli.store');
+    Route::post('/aemtli/update', [AemtliController::class, 'update'])->name('aemtli.update');
+    Route::get('/aemtli/destroy/{id}', [AemtliController::class, 'destroy'])->name('aemtli.destroy');
 });
 
 require __DIR__.'/auth.php';
