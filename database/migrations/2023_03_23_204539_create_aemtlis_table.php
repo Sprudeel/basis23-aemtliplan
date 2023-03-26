7a4e9a3c051e106620e635160e7bc46371954305
Léonard Schafer <79663902+Sprudeel@users.noreply.github.com>
2023-03-26 21:27:47 +0000

    feat(db): update columns

--- database/migrations/2023_03_23_204539_create_aemtlis_table.php
+++ database/migrations/2023_03_23_204539_create_aemtlis_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('aemtlis', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->foreignId('group')->default(0);
             $table->timestamps();
         });
     }
