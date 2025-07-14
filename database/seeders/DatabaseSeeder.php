3d906cb70454a0898c53ff85e8440cde45f34158
Léonard Schafer <79663902+Sprudeel@users.noreply.github.com>
2025-07-14 23:30:51 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,10 +15,10 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         \App\Models\User::factory()->create([
-            'name' => 'Sprudel',
-            'email' => 'sprudel@example.com',
+            'name' => 'Sueno',
+            'email' => 'aemtli@risola25.com',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
+            'password' => 'Hash::make('suenobuenorisola25')
         ]);
     }
 }
